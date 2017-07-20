4117a5e8baae75ce2322a936769ca9dc0c9506c0
rob <rwaddell91@googlemai.com>
2017-07-20 19:25:42 +0200

    First node js object: QueryResolver

---
+++ app/query_resolver/queryResolver.js
@@ -0,0 +1,30 @@
+function QueryResolver(mappingFile) {
+    this.parameterKeys = Object.keys(mappingFile);
+    this.mapping = mappingFile;
+}
+
+QueryResolver.prototype.getQueryHandlerFromParameters = function (queryParameters) {
+    var queryHandler = this.lookUpParameterSet(queryParameters);
+    return queryHandler;
+};
+
+QueryResolver.prototype.lookUpParameterSet = function (queryParameters) {
+        for (var i = 0; i < this.parameterKeys.length; i++) {
+            var found = true;
+            for (var j = 0; j < queryParameters.length; j++) {
+                if (this.parameterKeys[i].indexOf(queryParameters[j]) === -1) {
+                    found = false;
+                }
+            }
+
+            if (found) {
+                queryHandler = this.mapping[this.parameterKeys[i]];
+                return queryHandler;
+            }
+        }
+
+        return [];
+
+};
+
+module.exports = QueryResolver;
